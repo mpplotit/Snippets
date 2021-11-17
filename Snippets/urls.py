73fc62cba9bbd19541968f6c46af7660874388d6
mpplotit <mih_ak51@mail.ru>
2021-11-17 00:42:33 -0800

    Шаг 6

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.urls import path
 
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
-]
-
+                  path('', views.index_page, name='home'),
+                  path('snippets/add', views.add_snippet_page, name='snippets-add'),
+                  path('snippets/list', views.snippets_page, name='snippets-list'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

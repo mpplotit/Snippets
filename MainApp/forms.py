29d821804b6ee0ee282a615c6923e34da97fe465
mpplotit <mih_ak51@mail.ru>
2021-12-09 00:15:44 -0800

    modify forms

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,9 +1,19 @@
 from django.forms import ModelForm
 from MainApp.models import Snippet
+from django.forms import TextInput
 
 
 class SnippetForm(ModelForm):
-   class Meta:
-       model = Snippet
-       # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code','rate']
+    class Meta:
+        model = Snippet
+        # Описываем поля, которые будем заполнять в форме
+        fields = ['name', 'lang', 'code', 'rate']
+        labels = {
+            "name": ""
+        }
+        widgets = {
+            "name": TextInput(attrs={
+                "placeholder": "Название виджета",
+                "class": "name"
+            })
+        }

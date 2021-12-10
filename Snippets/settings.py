a46e1383d5d64b4e1610e496edae654a3a29b640
mpplotit <mih_ak51@mail.ru>
2021-12-10 01:03:22 -0800

    Setup heroku

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -83,6 +84,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -124,5 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
   BASE_DIR / "static"
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
